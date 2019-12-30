4df8eea40ff862e6ea7e1e79d367b507e29a78a6
George Obryadin <egor.obr@gmail.com>
2019-12-30 13:39:01 +0300

    chaged text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hi there! 23423423525
+          I was changed
         </p>
       </div>
     );
